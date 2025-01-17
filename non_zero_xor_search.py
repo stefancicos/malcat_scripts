bac19214ce889271aa875ae88a7c4c6babf7232f
stefancicos <77525072+stefancicos@users.noreply.github.com>
2025-01-17 10:52:06 +0200

    Update non_zero_xor_search.py

--- non_zero_xor_search.py
+++ non_zero_xor_search.py
@@ -18,5 +18,5 @@ for fn in analysis.fns:
 		if basic_block.code:
 			for insn in basic_block:
 				if insn.type == Instruction.Type.XOR:
-					gui.print(f"[rva]{insn.address:x}[/rva]: "+str(insn).replace("[","\[")+" ", format=True) # TODO: remove +" " after the bug is fixed
+					gui.print(f"[rva]{insn.address:x}[/rva]: "+str(insn).replace("[","\["), format=True) # In Malcat <= 0.9.8 you need to add an extra space at the end of the print (+" ") to work around a bug that is fixed since 0.9.9
 					analysis.comments[insn.address] = "Non Zero XOR"
