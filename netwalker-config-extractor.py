55cf6564ff0baa53299733b93482229c069a634e
stefancicos <77525072+stefancicos@users.noreply.github.com>
2025-02-09 14:07:57 +0200

    Update netwalker-config-extractor.py

--- netwalker-config-extractor.py
+++ netwalker-config-extractor.py
@@ -2,7 +2,9 @@ import malcat
 from transforms.stream import RC4
 malcat.setup()
 
-rsc = analysis.vfiles[0].open().read()
+for i in analysis.vfiles:
+	if i.path == "1337/31337/unk":
+		rsc = i.open().read()
 k_len = rsc[0]
 key = rsc[4:4+k_len]
 data = rsc[4+k_len:]
